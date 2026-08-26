cd7e65de25c6c38fae28792705cfa5ab14f2669b
Ipopqqx <bryan.klinski@gmail.com>
2026-08-25 20:24:34 -0500

    Fix automatic companion recruitment on game start

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -1,57 +1,22 @@
 extends Node
 
-var potions = 0
+var potions: int = 0
 var party: Array[String] = ["Aria"]
 var companions = {
-	"Elara": {
-		"recruited": false,
-		"bond": 0,
-		"captured": false,
-		"corruption_stage": 0
-	},
-	"Lyra": {
-		"recruited": false,
-		"bond": 0,
-		"captured": false,
-		"corruption_stage": 0
-	},
-	"Selene": {
-		"recruited": false,
-		"bond": 0,
-		"captured": false,
-		"corruption_stage": 0
-	},
-	"Vivienne": {
-		"recruited": false,
-		"bond": 0,
-		"captured": false,
-		"corruption_stage": 0
-	},
-	"Freya": {
-		"recruited": false,
-		"bond": 0,
-		"captured": false,
-		"corruption_stage": 0
-	}
+	"Elara": {"recruited": false, "bond": 0, "captured": false, "corruption_stage": 0},
+	"Lyra": {"recruited": false, "bond": 0, "captured": false, "corruption_stage": 0},
+	"Selene": {"recruited": false, "bond": 0, "captured": false, "corruption_stage": 0},
+	"Vivienne": {"recruited": false, "bond": 0, "captured": false, "corruption_stage": 0},
+	"Freya": {"recruited": false, "bond": 0, "captured": false, "corruption_stage": 0}
 }
 
-func recruit_companion(name: String):
-	if companions.has(name):
-		companions[name]["recruited"] = true
-
+func recruit_companion(name: String) -> void:
+	if not companions.has(name):
+		return
+	companions[name]["recruited"] = true
 	if not party.has(name):
 		party.append(name)
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GameState.recruit_companion("Elara")
-	GameState.recruit_companion("Lyra")
-	GameState.recruit_companion("Selene")
-	GameState.recruit_companion("Vivienne")
-	GameState.recruit_companion("Freya")
-	print(GameState.party)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Companions are recruited through gameplay, not automatically at startup.
+	print(party)
