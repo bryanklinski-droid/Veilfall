b6b6e3627cda68867216b151c4f80d31292286af
Ipopqqx <bryan.klinski@gmail.com>
2026-08-25 20:38:27 -0500

    Track consecutive defeats for capture rules

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -14,6 +14,7 @@ var corruption_stage: int = 0
 var corruption_days_remaining: int = 0
 var escape_defeats: int = 0
 var companionless_defeats: int = 0
+var consecutive_companion_defeats: int = 0
 var hero_captured: bool = false
 
 func recruit_companion(name: String) -> void:
@@ -30,6 +31,7 @@ func reset_new_game() -> void:
 	corruption_days_remaining = 0
 	escape_defeats = 0
 	companionless_defeats = 0
+	consecutive_companion_defeats = 0
 	hero_captured = false
 	for name in companions:
 		companions[name] = {
