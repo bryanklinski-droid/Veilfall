5ce52aa1a61073285b448d2cfcd177fd9ffab592
Ipopqqx <bryan.klinski@gmail.com>
2026-08-25 20:38:11 -0500

    Add persistent corruption and capture state

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -10,6 +10,12 @@ var companions = {
 	"Freya": {"recruited": false, "bond": 0, "captured": false, "corruption_stage": 0}
 }
 
+var corruption_stage: int = 0
+var corruption_days_remaining: int = 0
+var escape_defeats: int = 0
+var companionless_defeats: int = 0
+var hero_captured: bool = false
+
 func recruit_companion(name: String) -> void:
 	if not companions.has(name):
 		return
@@ -20,6 +26,11 @@ func recruit_companion(name: String) -> void:
 func reset_new_game() -> void:
 	potions = 0
 	party = ["Aria"]
+	corruption_stage = 0
+	corruption_days_remaining = 0
+	escape_defeats = 0
+	companionless_defeats = 0
+	hero_captured = false
 	for name in companions:
 		companions[name] = {
 			"recruited": false,
