633126899ae7e2af261e3058af4f8cebb9dd17fa
Ipopqqx <bryan.klinski@gmail.com>
2026-08-25 20:38:39 -0500

    Implement basic event state management

--- autoload/event_manager.gd
+++ autoload/event_manager.gd
@@ -1,11 +1,30 @@
 extends Node
 
+signal event_started(event_id: String)
+signal event_completed(event_id: String)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var completed_events: Dictionary = {}
+var active_event_id := ""
 
+func start_event(event_id: String) -> bool:
+	if event_id.is_empty() or active_event_id != "":
+		return false
+	active_event_id = event_id
+	event_started.emit(event_id)
+	return true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func complete_event(event_id: String = "") -> void:
+	var id := event_id if not event_id.is_empty() else active_event_id
+	if id.is_empty():
+		return
+	completed_events[id] = true
+	if active_event_id == id:
+		active_event_id = ""
+	event_completed.emit(id)
+
+func has_completed(event_id: String) -> bool:
+	return completed_events.get(event_id, false)
+
+func reset_events() -> void:
+	completed_events.clear()
+	active_event_id = ""
