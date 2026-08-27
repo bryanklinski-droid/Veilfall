64ee290e8f967da1a2ae9d860b44424a5767d077
Ipopqqx <bryan.klinski@gmail.com>
2026-08-27 14:34:20 -0500

    Fix treasure chest persistence and interaction

--- scenes/World/treasure_chest.gd
+++ scenes/World/treasure_chest.gd
@@ -3,26 +3,29 @@ extends Area2D
 @export var item_id: String = "cell_key"
 @export var amount: int = 1
 @export var chest_id: String = ""
-var opened = false
+var opened := false
 
 func _ready() -> void:
-	# Check if this chest was already opened in a previous session
-	opened = SaveManager.get_progress_flag("chest_" + chest_id)
+	add_to_group("interactable")
+	opened = GameState.is_chest_open(chest_id)
 	if opened:
 		_show_opened_state()
+
+func interact() -> void:
+	open_chest()
 
 func open_chest() -> void:
 	if opened:
 		return
-
 	opened = true
-	SaveManager.set_progress_flag("chest_" + chest_id, true)
 	InventoryManager.add_item(item_id, amount)
+	if not chest_id.is_empty():
+		GameState.record_chest_open(chest_id)
+	else:
+		SaveManager.save_game()
 	print("Found ", amount, " x ", item_id)
 	_show_opened_state()
-	SaveManager.save_game()
 
 func _show_opened_state() -> void:
-	"""Visual feedback that chest is opened"""
-	# TODO: Add animation or visual change when opened
+	# TODO: Add animation or sprite change when opened.
 	pass
