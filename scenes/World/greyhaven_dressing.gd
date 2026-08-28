8fdac977eda5f55846f432773af806deb15307b5
Ipopqqx <bryan.klinski@gmail.com>
2026-08-27 19:51:33 -0500

    Compose Greyhaven edges with layered walls and flower beds

--- scenes/World/greyhaven_dressing.gd
+++ scenes/World/greyhaven_dressing.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 const FOLIAGE := preload("res://assets/art/world/greyhaven_foliage_cluster.svg")
 const GROUND_DETAIL := preload("res://assets/art/world/greyhaven_ground_detail.svg")
+const FLOWER_BED := preload("res://assets/art/world/greyhaven_flower_bed.svg")
 const FENCE := preload("res://assets/art/world/greyhaven_fence.svg")
+const MOSSY_WALL := preload("res://assets/art/world/greyhaven_mossy_wall.svg")
 const CORRUPT_GROWTH := preload("res://assets/art/world/greyhaven_corrupt_growth.svg")
 const ROADSIDE_CLUTTER := preload("res://assets/art/world/greyhaven_roadside_clutter.svg")
 const LANTERN := preload("res://assets/art/world/greyhaven_lantern.svg")
@@ -15,6 +17,7 @@ func _ready() -> void:
 	_add_road_wear()
 	_add_cobbles()
 	_add_boundaries()
+	_add_composed_edges()
 	_add_road_edge_growth()
 	_add_foliage()
 	_add_roadside_props()
@@ -40,27 +43,26 @@ func _patch(points: PackedVector2Array, color: Color) -> void:
 
 func _add_ground_layers() -> void:
 	var normal_detail := [
-		[Vector2(180, 300), 0.62, -0.08], [Vector2(410, 405), 0.78, 0.05],
-		[Vector2(650, 310), 0.58, -0.03], [Vector2(230, 700), 0.70, 0.06],
-		[Vector2(505, 825), 0.82, -0.05], [Vector2(720, 1080), 0.62, 0.08],
-		[Vector2(1250, 350), 0.55, -0.04], [Vector2(1320, 1080), 0.60, 0.05]
+		[Vector2(180,300),0.62,-0.08],[Vector2(410,405),0.78,0.05],[Vector2(650,310),0.58,-0.03],
+		[Vector2(230,700),0.70,0.06],[Vector2(505,825),0.82,-0.05],[Vector2(720,1080),0.62,0.08],
+		[Vector2(1250,350),0.55,-0.04],[Vector2(1320,1080),0.60,0.05]
 	]
 	for data in normal_detail:
-		_sprite(GROUND_DETAIL, data[0], data[1], Color(0.82, 0.88, 0.72, 0.72), data[2])
+		_sprite(GROUND_DETAIL, data[0], data[1], Color(0.82,0.88,0.72,0.62), data[2])
 	var sick_detail := [
-		[Vector2(1450, 380), 0.62, 0.04], [Vector2(1510, 610), 0.72, -0.06],
-		[Vector2(1580, 875), 0.66, 0.07], [Vector2(1740, 990), 0.58, -0.03]
+		[Vector2(1450,380),0.62,0.04],[Vector2(1510,610),0.72,-0.06],
+		[Vector2(1580,875),0.66,0.07],[Vector2(1740,990),0.58,-0.03]
 	]
 	for data in sick_detail:
-		_sprite(GROUND_DETAIL, data[0], data[1], Color(0.50, 0.48, 0.42, 0.58), data[2])
+		_sprite(GROUND_DETAIL, data[0], data[1], Color(0.50,0.48,0.42,0.50), data[2])
 
 func _embedded_stone(pos: Vector2, size: Vector2, rotation_value: float, stone_color: Color) -> void:
 	var shadow := Polygon2D.new()
 	shadow.polygon = PackedVector2Array([Vector2(-0.52,-0.25),Vector2(-0.24,-0.50),Vector2(0.24,-0.47),Vector2(0.53,-0.16),Vector2(0.43,0.30),Vector2(0.08,0.49),Vector2(-0.38,0.35)])
-	shadow.position = pos + Vector2(1.0, 1.5)
+	shadow.position = pos + Vector2(1.0,1.5)
 	shadow.scale = size
 	shadow.rotation = rotation_value
-	shadow.color = Color(0.07, 0.06, 0.05, 0.16)
+	shadow.color = Color(0.07,0.06,0.05,0.16)
 	add_child(shadow)
 	var stone := Polygon2D.new()
 	stone.polygon = PackedVector2Array([Vector2(-0.52,-0.28),Vector2(-0.25,-0.50),Vector2(0.23,-0.46),Vector2(0.52,-0.14),Vector2(0.41,0.28),Vector2(0.07,0.47),Vector2(-0.39,0.33)])
@@ -72,16 +74,19 @@ func _embedded_stone(pos: Vector2, size: Vector2, rotation_value: float, stone_c
 
 func _add_road_wear() -> void:
 	var patches := [
-		[PackedVector2Array([Vector2(928,250),Vector2(1018,238),Vector2(1042,325),Vector2(947,344)]), Color(0.18,0.12,0.08,0.10)],
-		[PackedVector2Array([Vector2(934,500),Vector2(1024,486),Vector2(1055,590),Vector2(945,612)]), Color(0.12,0.09,0.07,0.09)],
-		[PackedVector2Array([Vector2(920,665),Vector2(1040,650),Vector2(1060,740),Vector2(925,755)]), Color(0.22,0.16,0.10,0.09)],
-		[PackedVector2Array([Vector2(725,718),Vector2(905,715),Vector2(912,802),Vector2(735,810)]), Color(0.16,0.11,0.08,0.08)],
-		[PackedVector2Array([Vector2(1090,713),Vector2(1320,705),Vector2(1328,790),Vector2(1098,798)]), Color(0.14,0.10,0.07,0.08)],
-		[PackedVector2Array([Vector2(936,980),Vector2(1030,968),Vector2(1050,1080),Vector2(942,1092)]), Color(0.18,0.12,0.08,0.09)]
+		[PackedVector2Array([Vector2(928,250),Vector2(1018,238),Vector2(1042,325),Vector2(947,344)]),Color(0.18,0.12,0.08,0.10)],
+		[PackedVector2Array([Vector2(934,500),Vector2(1024,486),Vector2(1055,590),Vector2(945,612)]),Color(0.12,0.09,0.07,0.09)],
+		[PackedVector2Array([Vector2(920,665),Vector2(1040,650),Vector2(1060,740),Vector2(925,755)]),Color(0.22,0.16,0.10,0.09)],
+		[PackedVector2Array([Vector2(725,718),Vector2(905,715),Vector2(912,802),Vector2(735,810)]),Color(0.16,0.11,0.08,0.08)],
+		[PackedVector2Array([Vector2(1090,713),Vector2(1320,705),Vector2(1328,790),Vector2(1098,798)]),Color(0.14,0.10,0.07,0.08)],
+		[PackedVector2Array([Vector2(936,980),Vector2(1030,968),Vector2(1050,1080),Vector2(942,1092)]),Color(0.18,0.12,0.08,0.09)]
 	]
 	for data in patches:
 		_patch(data[0], data[1])
-	for data in [[PackedVector2Array([Vector2(954,0),Vector2(962,260),Vector2(956,520),Vector2(966,790),Vector2(958,1060),Vector2(968,1536)]),2.0,Color(0.10,0.07,0.05,0.12)],[PackedVector2Array([Vector2(1010,0),Vector2(1004,270),Vector2(1014,540),Vector2(1006,795),Vector2(1018,1070),Vector2(1010,1536)]),2.0,Color(0.10,0.07,0.05,0.10)]]:
+	for data in [
+		[PackedVector2Array([Vector2(954,0),Vector2(962,260),Vector2(956,520),Vector2(966,790),Vector2(958,1060),Vector2(968,1536)]),2.0,Color(0.10,0.07,0.05,0.12)],
+		[PackedVector2Array([Vector2(1010,0),Vector2(1004,270),Vector2(1014,540),Vector2(1006,795),Vector2(1018,1070),Vector2(1010,1536)]),2.0,Color(0.10,0.07,0.05,0.10)]
+	]:
 		var line := Line2D.new()
 		line.points = data[0]
 		line.width = data[1]
@@ -100,7 +105,7 @@ func _add_cobbles() -> void:
 		[Vector2(1008,1146),Vector2(13,8),-0.15],[Vector2(974,1272),Vector2(12,7),0.12]
 	]
 	for data in stones:
-		_embedded_stone(data[0], data[1], data[2], Color(0.25, 0.25, 0.22, 0.44))
+		_embedded_stone(data[0], data[1], data[2], Color(0.25,0.25,0.22,0.44))
 
 func _add_boundaries() -> void:
 	for data in [
@@ -111,6 +116,37 @@ func _add_boundaries() -> void:
 	]:
 		var fence := _sprite(FENCE, data[0], data[1], data[3], data[2])
 		fence.z_index = 1
+
+func _add_composed_edges() -> void:
+	# Larger, overlapping environment masses replace the old "objects on a lawn" feeling.
+	for data in [
+		[Vector2(390,640),0.62,-0.04,Color(0.74,0.77,0.64,0.88)],
+		[Vector2(645,1120),0.56,0.03,Color(0.66,0.70,0.58,0.82)],
+		[Vector2(1320,520),0.54,0.02,Color(0.61,0.62,0.54,0.78)],
+		[Vector2(1505,1060),0.50,-0.03,Color(0.48,0.47,0.43,0.72)]
+	]:
+		var wall := _sprite(MOSSY_WALL, data[0], data[1], data[3], data[2])
+		wall.z_index = 1
+
+	for data in [
+		[Vector2(450,570),0.50,-0.05,Color(0.86,0.87,0.70,0.90)],
+		[Vector2(650,630),0.42,0.04,Color(0.78,0.80,0.64,0.84)],
+		[Vector2(430,1080),0.56,0.03,Color(0.84,0.84,0.68,0.88)],
+		[Vector2(1250,500),0.42,-0.02,Color(0.72,0.73,0.60,0.80)],
+		[Vector2(1370,980),0.46,0.05,Color(0.60,0.57,0.54,0.72)]
+	]:
+		var bed := _sprite(FLOWER_BED, data[0], data[1], data[3], data[2])
+		bed.z_index = 2
+
+	# A few overlapping trees anchor the masses instead of dotting trees uniformly across the field.
+	for data in [
+		[Vector2(330,520),0.92,-0.03,Color(0.84,0.90,0.82,0.96)],
+		[Vector2(510,500),0.72,0.02,Color(0.76,0.83,0.73,0.92)],
+		[Vector2(1450,430),0.82,0.03,Color(0.56,0.59,0.53,0.82)],
+		[Vector2(1580,500),0.68,-0.02,Color(0.47,0.48,0.46,0.76)]
+	]:
+		var tree := _sprite(PINE, data[0], data[1], data[3], data[2])
+		tree.z_index = 3
 
 func _add_road_edge_growth() -> void:
 	var edge_growth := [[Vector2(900,315),0.22],[Vector2(1070,405),0.18],[Vector2(895,590),0.20],[Vector2(1080,625),0.16],[Vector2(888,930),0.20],[Vector2(1082,1035),0.18],[Vector2(530,680),0.18],[Vector2(700,820),0.16],[Vector2(1260,680),0.17],[Vector2(1440,825),0.15],[Vector2(1640,690),0.14]]
@@ -124,7 +160,7 @@ func _add_foliage() -> void:
 		[Vector2(1320,570),0.54],[Vector2(1185,420),0.40],[Vector2(1240,530),0.34],[Vector2(1170,610),0.28]
 	]
 	for data in spots:
-		_sprite(FOLIAGE, data[0], data[1], Color(0.76,0.86,0.70,0.82))
+		_sprite(FOLIAGE, data[0], data[1], Color(0.76,0.86,0.70,0.72))
 
 func _add_roadside_props() -> void:
 	for data in [[Vector2(835,360),0.66],[Vector2(1135,510),0.56],[Vector2(830,1120),0.60],[Vector2(1160,1240),0.66]]:
@@ -151,16 +187,15 @@ func _add_corruption_transition() -> void:
 	]:
 		_sprite(FOLIAGE, data[0], data[1], data[2])
 	for data in [
-		[Vector2(1505,525),0.58,-0.16,Color(0.72,0.52,0.72,0.58)],
-		[Vector2(1630,650),0.72,0.10,Color(0.88,0.62,0.90,0.72)],
-		[Vector2(1740,790),0.86,-0.08,Color(1.0,0.72,1.0,0.84)],
-		[Vector2(1810,960),0.74,0.16,Color(0.90,0.58,0.92,0.76)]
+		[Vector2(1505,525),0.58,-0.16,Color(0.64,0.50,0.64,0.48)],
+		[Vector2(1630,650),0.72,0.10,Color(0.72,0.54,0.74,0.58)],
+		[Vector2(1740,790),0.86,-0.08,Color(0.82,0.60,0.84,0.66)],
+		[Vector2(1810,960),0.74,0.16,Color(0.76,0.52,0.78,0.62)]
 	]:
 		var growth := _sprite(CORRUPT_GROWTH, data[0], data[1], data[3], data[2])
 		growth.z_index = 1
 
 func _add_foreground_frame() -> void:
-	# Larger edge silhouettes create foreground depth without covering the central playable space.
 	for data in [
 		[Vector2(85,1390),1.48,-0.04,Color(0.70,0.78,0.70,0.90)],
 		[Vector2(260,1460),1.15,0.02,Color(0.66,0.74,0.66,0.86)],
